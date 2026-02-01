4c76e9d99a3d2246b37608c7c4d6a13439d4e642
kirilsSurovovs <kirils.surovovs@gmail.com>
2026-02-01 18:41:33 +0200

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Replace with your actual repo name
+  base: '/ealatvia-website', // Replace with your actual repo name
 })
