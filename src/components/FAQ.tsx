efb5dde7e708674efd92bad935b46f89262f7efe
kirilsSurovovs <kirils.surovovs@gmail.com>
2026-04-06 21:34:57 +0300

    Three new questions added in FAQ

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -10,6 +10,10 @@ const FAQ: React.FC = () => {
     { question: t('faq.q1'), answer: t('faq.a1') },
     { question: t('faq.q2'), answer: t('faq.a2') },
     { question: t('faq.q3'), answer: t('faq.a3') },
+    { question: t('faq.q4'), answer: t('faq.a4') },
+    { question: t('faq.q5'), answer: t('faq.a5') },
+    { question: t('faq.q6'), answer: t('faq.a6') },
+    { question: t('faq.q7'), answer: t('faq.a7') },
   ];
 
   const toggle = (index: number) => {
